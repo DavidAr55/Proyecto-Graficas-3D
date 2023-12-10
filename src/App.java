19c9b4fa1ac6283d44f473b71b7abc25ad49c08b
DavidAr55 <davidarturoloera@gmail.com>
2023-12-10 10:56:05 -0600

    Transformaciones de rotacion

--- src/App.java
+++ src/App.java
@@ -1,6 +1,38 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Poligon3D Poligon3D = new Poligon3D(0, 0, 0);
-        Poligon3D.drawPoligon3D();
+        int[] center = { 10, -100, 10 };
+        int[] proyectionCenter = { 1, -5, 5 };
+
+        int[][] pX = { 
+            { -100, -100, 100, 100, -100, -100, 100, 100 },
+            { -100, -100, 100, 100, -100, -100, 100, 100 } };
+
+        int[][] pY = { 
+            { -200, 200, -200, 200, -200, 200, -200, 200 },
+            { 200, 450, 200, 450, -450, -200, -450, -200 } };
+
+        int[][] pZ = { 
+            { -100, -100, -100, -100, 100, 100, 100, 100 },
+            { -100, -100, -100, -100, 100, 100, 100, 100 } };
+        double scale =10;
+
+        Poligon3D poligon3D = new Poligon3D(center, proyectionCenter, pX, pY, pZ, scale);
+        poligon3D.drawCenterPoigon3D();
+        poligon3D.drawPoligon3D();
+        poligon3D.rotateAndShow(0, 0.1, 0, 50);
+        /*for(int i = 0; i < 360; i++) {
+            try {
+                
+                int[] auxPC = {1 + i, -5, 5};
+
+                poligon3D.drawPoligon3D();
+                poligon3D.setProyectionCenter(auxPC);
+                Thread.sleep(100);
+                poligon3D.clearScreen();
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }*/
     }
 }
--- src/Poligon3D.java
+++ src/Poligon3D.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Poligon3D extends Screen {
 
@@ -10,24 +11,34 @@ public class Poligon3D extends Screen {
     private int yCenter;
     private int zCenter;
 
+    private int[] proyectionCenter;
+
     private int[][] xPoligon3D;
     private int[][] yPoligon3D;
     private int[][] zPoligon3D;
+
+    private double scale;
 
     private int[] poligonFaces;
     private int[][] cubeFace;
     private Color[] colors;
 
-    public Poligon3D(int xC, int yC, int zC) {
+    private ArrayList<int[]> polygonPoints = new ArrayList<>();
+
+    public Poligon3D(int[] C, int[] pC, int[][] xP, int[][] yP, int[][] zP, double scale) {
         super();
 
-        this.xCenter = xC;
-        this.yCenter = yC;
-        this.zCenter = zC;
+        this.xCenter = C[0];
+        this.yCenter = C[1];
+        this.zCenter = C[2];
 
-        this.xPoligon3D = new int[][] {{-50, 50, 50, -50}};
-        this.yPoligon3D = new int[][] {{-50, 50, 50, -50}};
-        this.zPoligon3D = new int[][] {{-50, 50, 50, -50}};
+        this.proyectionCenter = pC;
+
+        this.xPoligon3D = xP;
+        this.yPoligon3D = yP;
+        this.zPoligon3D = zP;
+
+        this.scale = scale;
 
         this.cubeFace = new int[][] {
             {0, 1, 2, 3},  // Cara frontal
@@ -39,15 +50,21 @@ public class Poligon3D extends Screen {
         };     
         
         this.colors = new Color[] {
-            Color.RED,      // Color de la cara frontal
-            Color.BLUE,     // Color de la cara trasera
-            Color.GREEN,    // Color de la cara izquierda
-            Color.YELLOW,   // Color de la cara derecha
-            Color.WHITE,    // Color de la cara superior
-            Color.ORANGE    // Color de la cara inferior
+            Color.RED,      // 0 Abajo -> 2
+            Color.BLUE,     // 1 Arriba -> 2
+            Color.RED,      // 2 Abajo -> 1
+            Color.BLUE,     // 3 Arriba -> 1
+            Color.BLUE,     // 4 Arriba -> 0
+            Color.RED,      // 5 Abajo -> 0
+            Color.BLUE,     // 6 Arriba -> 3
+            Color.RED       // 7 Abajo -> 3
         };
 
         initBuffer();
+    }
+
+    public void setProyectionCenter(int[] proyectionCenter) {
+        this.proyectionCenter = proyectionCenter;
     }
 
     public void initBuffer() {
@@ -57,6 +74,25 @@ public class Poligon3D extends Screen {
         Graphics2D g2d = (Graphics2D) graPixel;
         g2d.setBackground(Color.WHITE);
         g2d.clearRect(0, 0, getWidth(), getHeight());
+    }
+
+    public void drawCenterPoigon3D() {
+        int x, y, z;
+        double u;
+
+        u = this.proyectionCenter[2];
+        u /= (zCenter - this.proyectionCenter[2]);
+
+        // Cálculo de las coordenadas x e y
+        x = (int) ((getWidth() / 2) + (this.proyectionCenter[0] + (xCenter - this.proyectionCenter[0]) * u));
+        y = (int) ((getHeight() / 2) + (this.proyectionCenter[1] + (yCenter - this.proyectionCenter[1]) * u));
+        z = (int) ((getWidth() / 2) + (this.proyectionCenter[2] + (zCenter - this.proyectionCenter[2]) * u));
+
+        xCenter = x;
+        yCenter = y;
+        zCenter = z;
+
+        fillEllipseScanLine(xCenter, yCenter, 5, 5, Color.GREEN);
     }
 
     public void clearScreen() {
@@ -95,12 +131,116 @@ public class Poligon3D extends Screen {
         }
     }
 
-    public void drawPoligon3D() {
+    public void fillEllipseScanLine(int centerX, int centerY, int a, int b, Color fillColor) {
         
+        double a2 = a * a;
+        double b2 = b * b;
+    
+        for (int y = -b; y <= b; y++) {
+            int xLimit = (int) (a * Math.sqrt(1 - (y * y) / b2));
+            for (int x = -xLimit; x <= xLimit; x++) {
+                int pixelX = centerX + x;
+                int pixelY = centerY + y;
+                putPixel(pixelX, pixelY, fillColor);
+            }
+        }
+
+        repaint();
+    }
+
+    public void drawPoligon3D() {
+    
+        for (int i = 0; i < this.xPoligon3D.length; i++) {
+            for (int j = 0; j < this.xPoligon3D[i].length; j++) {
+                int x, y;
+                double u;
+    
+                u = this.proyectionCenter[2];
+                u /= (zPoligon3D[i][j] - this.proyectionCenter[2]);
+    
+                // Cálculo de las coordenadas x e y
+                x = (int) ((getWidth() / 2) + ((this.proyectionCenter[0] + (xPoligon3D[i][j] - this.proyectionCenter[0]) * u) * scale));
+                y = (int) ((getHeight() / 2) + ((this.proyectionCenter[1] + (yPoligon3D[i][j] - this.proyectionCenter[1]) * u) * scale));
+
+                // Guarda las coordenadas en un array para conectar los vértices posteriormente
+                int[] coordinates = new int[]{x, y};
+                // Agrega las coordenadas al array de puntos del polígono
+                polygonPoints.add(coordinates);
+    
+                fillEllipseScanLine(x, y, 5, 5, this.colors[j]);
+            }
+
+            // Une los vértices para formar el polígono completo
+            connectPolygonVertices(this.colors[i]);
+            polygonPoints.clear();
+        }
+
+        repaint();
+    }
+    
+    private void connectPolygonVertices(Color color) {
+        int numVertices = polygonPoints.size();
+    
+        for (int i = 0; i < numVertices; i++) {
+            int[] p1 = polygonPoints.get(i);
+            for (int j = 0; j < numVertices; j++) {
+                if (i != j) {
+                    int[] p2 = polygonPoints.get(j);
+                    drawLineBresenham(p1[0], p1[1], p2[0], p2[1], color);
+                }
+            }
+        }
+    
+        repaint();
+    }
+
+    public void rotateX(double angle) {
+        double[][] matrixX = 
+            {{1,  0,               0,               0},
+             {0,  Math.cos(angle), Math.sin(angle), 0},
+             {0, -Math.sin(angle), Math.cos(angle), 0},
+             {0,  0,               0,               1}}; 
+    }
+    
+    public void rotateY(double angle) {
+        double[][] matrixY = 
+            {{Math.cos(angle), 0, -Math.sin(angle), 0},
+             {0,               1,  0,               0},
+             {Math.sin(angle), 0,  Math.cos(angle), 0},
+             {0,               0,  0,               1}}; 
+    }
+    
+    public void rotateZ(double angle) {
+        double[][] matrixZ = 
+            {{ Math.cos(angle), Math.sin(angle), 0, 0},
+             {-Math.sin(angle), Math.cos(angle), 0, 0},
+             {0,                0,               1, 0},
+             {0,                0,               0, 1}}; 
     }
     
     
-      
+    // Método para rotar y mostrar los polígonos en cada eje
+    public void rotateAndShow(double angleX, double angleY, double angleZ, int delay) {
+        for (int i = 0; i < 360; i ++) {
+            clearScreen();
+            
+            // Aplicar rotaciones en cada eje
+            rotateX(angleX);
+            rotateY(angleY);
+            rotateZ(angleZ);
+            
+            // Dibujar el polígono 3D
+            drawPoligon3D();
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            repaint();
+        }
+    }
 
     @Override
     public void paint(Graphics g) {
